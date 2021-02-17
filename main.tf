b5cf2a59e0919713d1cc0acd01ba4989152092f9
Anmol Nagpal <anmol@clouddrove.com>
2021-02-17 16:15:52 +0530

    added tags in label

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
 #              for resources. You can use terraform-labels to implement a strict naming
 #              convention.
 module "labels" {
-  source = "git::https://github.com/clouddrove/terraform-labels.git"
+  source = "git::https://github.com/clouddrove/terraform-labels.git?ref=tags/0.12.0"
 
   enabled     = var.enabled
   name        = var.name
